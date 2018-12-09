51b50e7cb8e084f4b9620477716f0965dffaae23
Tuấn Vương <vanhtuan0409@gmail.com>
2018-12-09 17:19:03 +0700

    exec command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
 
 func main() {
@@ -10,8 +11,27 @@ func main() {
 
 	switch cmd {
 	case "run":
-		fmt.Println("run")
+		run(os.Args[2], os.Args[3:])
 	default:
 		fmt.Println("Unknown command")
+	}
+}
+
+func run(command string, opts []string) {
+	cmd := exec.Command(command, opts...)
+
+	bindCmdStdio(cmd)
+	must(cmd.Run())
+}
+
+func bindCmdStdio(cmd *exec.Cmd) {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
