281bcd4587b4c92c8e898f34d0adec79b3505787
Tuấn Vương <vanhtuan0409@gmail.com>
2018-12-09 18:06:12 +0700

    setup pid isolation

--- main.go
+++ main.go
@@ -1,30 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
-func main() {
-	cmd := os.Args[1]
+var (
+	internal = flag.Bool("internal", false, "Mark process as internal isolation")
+)
 
+func main() {
+	flag.Parse()
+
+	cmd := os.Args[1]
 	switch cmd {
 	case "run":
+		setupIsolation(os.Args[2], os.Args[3:])
+	case "internal":
 		run(os.Args[2], os.Args[3:])
 	default:
 		fmt.Println("Unknown command")
 	}
 }
 
-func run(command string, opts []string) {
-	cmd := exec.Command(command, opts...)
+func setupIsolation(command string, opts []string) {
+	appendedArgs := append([]string{"internal"}, os.Args[2:]...)
+	cmd := exec.Command("/proc/self/exe", appendedArgs...)
 	bindCmdStdio(cmd)
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
+	must(cmd.Run())
+}
+
+func run(command string, opts []string) {
+	fmt.Printf("Running command %s as PID %d\n", command, os.Getpid())
+	cmd := exec.Command(command, opts...)
+	bindCmdStdio(cmd)
 	must(cmd.Run())
 }
 
